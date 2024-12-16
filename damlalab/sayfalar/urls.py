9746f55dc1889d586156a957d531b265c1314fe1
Burak Duvakli <burakduvakli@hotmail.com>
2024-12-16 23:10:51 +0300

    add new pages and update navigation for research group, positions, theses, projects, and contact

--- damlalab/sayfalar/urls.py
+++ damlalab/sayfalar/urls.py
@@ -4,4 +4,9 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('research/', views.research, name='research'),
+    path('research_group/', views.research_group, name='research_group'),
+    path('positions/', views.positions, name='positions'),
+    path('thesis/', views.thesis, name='thesis'),
+    path('projects/', views.projects, name='projects'),
+    path('contact/', views.contact, name='contact'),
 ]
--- damlalab/sayfalar/views.py
+++ damlalab/sayfalar/views.py
@@ -5,3 +5,18 @@ def index(request):
 
 def research(request):
     return render(request, 'research.html')
+
+def research_group(request):
+    return render(request, 'researchgroup.html')
+
+def positions(request):
+    return render(request, 'positions.html')
+
+def thesis(request):
+    return render(request, 'theses.html')
+
+def projects(request):
+    return render(request, 'projects.html')
+
+def contact(request):
+    return render(request, 'contact.html')
